3f8e932840b4f75e208e9d5367623dfebba4cb62
Damon Jablons <damonjablons@gmail.com>
2016-06-22 19:31:14 -0400

    Config formatting

--- 00-setup-db.sh
+++ 00-setup-db.sh
@@ -5,11 +5,15 @@ set -e
 
 # Install our custom config
 cat >> "$PGDATA/postgresql.conf" <<EOS
-# Custom config
+
+# - howgood/postgres - #
+# - : custom config  - #
+
 listen_addresses = '*'
 exit_on_error = off
 restart_after_crash = on
 external_pid_file = '/tmp/postgresql.pid'
+
 EOS
 
 # If extensions are listed, create them
