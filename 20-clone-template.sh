5dd8fdc46519a0f258525ad1443ed6932c8af235
Damon Jablons <damonjablons@gmail.com>
2016-06-22 20:04:08 -0400

    Added more startup config

---
+++ 20-clone-template.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -e
+
+
+# Create all of the extensions on template1
+for extension in $POSTGRES_EXTENSIONS; do
+  psql \
+    --echo-queries \
+    --set ON_ERROR_STOP=1 \
+    --username "$POSTGRES_USER" \
+    --database template1 \
+    --command \
+      "CREATE EXTENSION $extension ;"
+done
--- Dockerfile
+++ Dockerfile
@@ -2,4 +2,5 @@
 
 FROM howgood/postgres:latest
 
-COPY 10-add-to-conf.sh /docker-entrypoint-initdb.d/10-add-to-conf.sh
+COPY 10-add-to-conf.sh /docker-entrypoint-initdb.d/
+COPY 20-clone-template.sh /docker-entrypoint-initdb.d/
